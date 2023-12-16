46fd6a2aa289b66128c17b5330c99a54e0c33c80
MrGeomancer <101983215+MrGeomancer@users.noreply.github.com>
2023-12-16 16:44:36 +0500

    починил
    Он почему то перестал работать, возможно из-за обновления pyautogui, создал requirements чтобы такого не повторялось, починил, добавил комментраии, сделал более читаемым, кажтся

--- audioswitch.py
+++ audioswitch.py
@@ -1,33 +1,48 @@
 import pyautogui
 import time
-def switch():
-    # pyautogui.confirm(text='', title='', buttons=['вклюяить', 'Выключить'])
-    pos = pyautogui.position()
-    pyautogui.moveTo(1730, 1065, 0.15)
-    # pyautogui.moveTo(1730, 1065)
-    pyautogui.click(1730, 1065, button='right')
+import traceback
+
+
+def switch():  # выполняемая функция
+    # запомнить начальную позицию мыши, чтобы потом вернуть её туда
+    start_mouse_position = pyautogui.position()
+    # нажать пкм по причному расположению динамиков через 0,15 секунд после прошлого действия
+    pyautogui.click(1730, 1065, button='right', duration=0.15)
+    # нажать на "звуки"
     pyautogui.click(1730, 1010)
-    time.sleep(0.3)
-    soundwindow = pyautogui.locateOnScreen(r'png\soundwindow.png',confidence=0.8)
-    # print(soundwindow)
-    if soundwindow == None:
-        soundwindow = pyautogui.locateOnScreen(r'png\soundwindow.png',confidence=0.8)
-        # print(soundwindow)
-    pyautogui.click(soundwindow[0]+10, soundwindow[1]+45)
+    # подождать 0,3 секунды до появления окна звуков
+    time.sleep(0.33)
+    # найти на всём экране расположения окна звуков
+    soundwindow = pyautogui.locateOnScreen(r'png\soundwindow.png', confidence=0.8)
+
+    if soundwindow == None:  # в случае если он не нашел, попробует еще раз
+        soundwindow = pyautogui.locateOnScreen(r'png\soundwindow.png', confidence=0.8)
+
+    # открыть окно "воспроизведения"
+    pyautogui.click(soundwindow[0] + 10, soundwindow[1] + 45)
+    # подождать 0,2 секунды прогрузки
     time.sleep(0.2)
-    speaker_on = pyautogui.locateOnScreen(r'png\\speakeron.png',confidence=0.95)
-    if speaker_on != None:
-        pyautogui.click(pyautogui.center(speaker_on), button='right')
-        pyautogui.click(speaker_on[0]+50, speaker_on[1]+75)
-    else:
-        pyautogui.click(soundwindow[0]+100, soundwindow[1]+100, button='right')
-        pyautogui.click(soundwindow[0] + 150, soundwindow[1] + 140)
-        # pyautogui.click(speaker_on[0] + 50, speaker_on[1] + 75)
+    # найти иконку включенных динамиков
+    try:
+        speaker_on = pyautogui.locateOnScreen(r'png\\speakeron.png', confidence=0.95)
+    except pyautogui.ImageNotFoundException as e:
+        speaker_on = None
+
+    if speaker_on is not None:  # если икона включенных динамиков есть, то:
+        pyautogui.click(pyautogui.center(speaker_on), button='right')  # нажать по ней ПКМ
+        pyautogui.click(speaker_on[0] + 50, speaker_on[1] + 75)  # нажать "Отключить"
+    else:  # если иконки нет
+        pyautogui.click(soundwindow[0] + 100, soundwindow[1] + 100, button='right')  # нажать пкм по первому устройсву
+        pyautogui.click(soundwindow[0] + 150, soundwindow[1] + 140)  # включить его
+    # нажать на кнопку закрытия окна
     pyautogui.click(soundwindow[0] + 440, soundwindow[1] + 5)
-    pyautogui.moveTo(pos)
-    # pyautogui.moveTo(150, 150, 0.15)
-    # pyautogui.click()
-    # speaker = pyautogui.locateOnScreen('speakeroff.png')
+    # вернуть курсор на начальную позицию
+    pyautogui.moveTo(start_mouse_position)
+
+
 if __name__ == '__main__':
-    switch()
-#1732 1067
+    try:
+        switch()
+    except Exception as e:
+        print('Ошибка:\n', traceback.format_exc())
+# 1732 1067
